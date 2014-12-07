cb5cd73bbc33baa43904e241092a83b21d840ca9
belevtsoff <belevtsoff@gmail.com>
2014-12-08 00:53:39 +0100

    changed focus to wrapping DTI routines

--- nipype_wrapper_interfaces.py
+++ nipype_wrapper_interfaces.py
@@ -1,6 +1,3 @@
-"""A set of nipype's Function interface implementations for machine learning
-applications and corresponding earlPipeline wrappers for them"""
-
 # Nipype wrapper base classes
 from nipype_wrapper_base import NipypeWrapperUnit as Unit
 from nipype_wrapper_base import NipypeWrapperPipeline as Pipeline
@@ -9,205 +6,286 @@ from nipype_wrapper_base import NipypeWrapperPipeline as Pipeline
 from earlpipeline.backends.base import Parameter
 
 # Nipype API
-from nipype.interfaces.utility import Function
-from nipype.pipeline.engine import Workflow, Node, MapNode
-
-# common import strings
-COMMON_IMPORTS = ['import logging', 'import time', 'import os', 'import numpy as np']
-MVPA_IMPORTS = ['from mvpa2 import suite']
-NBACK_IMPORTS = ['import util', 'from nback_io import get_data']
-IMPORTS = COMMON_IMPORTS + MVPA_IMPORTS + NBACK_IMPORTS
+import nipype.interfaces.io as nio
+import nipype.interfaces.fsl as fsl
+import nipype.interfaces.utility as util
+import nipype.pipeline.engine as pe
+import os
+from nipype.workflows.dmri.fsl.dti import create_eddy_correct_pipeline, create_bedpostx_pipeline
 
 # parameter shortcuts
 def text_parameter(name, default):
     return Parameter(name, 'input', str, default, datatype='text')
 def float_parameter(name, default):
     return Parameter(name, 'input', float, default, datatype='number')
+def int_parameter(name, default):
+    return Parameter(name, 'input', int, default, datatype='number')
+def boolean_parameter(name, default):
+    return Parameter(name, 'boolean', bool, default)
 
-# ---------------------------------------------------------------------------
-# Range Source
-# ---------------------------------------------------------------------------
 
-def generate_data(N):
-    return range(N)
+##############################################################################
+# PrimitiveSource
+##############################################################################
 
-range_iface = Function(input_names=['N'],
-                 output_names=['list'],
-                 function = generate_data)
+I = util.IdentityInterface(fields=['str_par', 'float_par', 'int_par', 'bool_par'])
 
-class RangeSource(Unit):
-    interface = range_iface
-    tag = 'Sources'
+class PrimitiveSource(Unit):
+    interface = I
+    tag = "Sources"
+    instance_name_template = "primsrc"
 
-    N = Parameter(name='N',
-            parameter_type='input',
-            value_type=int,
-            default_value=10,
-            datatype='number')
-
-# ---------------------------------------------------------------------------
-# Nback Source
-# ---------------------------------------------------------------------------
-
-def reader_func(logger_name, data_path, condition, subject):
-    logger = logging.getLogger(logger_name)
-    logger.info("Loading data...")
-
-    ds = get_data(condition, subject, data_path)
-
-    ds.a.imghdr.set_data_dtype(np.dtype('int16'))
-    ds.sa['chunks_balanced'] = ds.chunks % 4
-
-    logger.info('Got data')
-
-    return ds
-
-reader_iface = Function(input_names=['logger_name', 'data_path', 'condition', 'subject'],
-        output_names=['data'],
-        function=reader_func,
-        imports=IMPORTS)
-
-class NbackSource(Unit):
-    interface = reader_iface
-    tag = 'Sources'
-
-    data_path = text_parameter('data_path', '/mnt/antares_raid/groups/norway/Nback/nipype_wrapper/data')
-    condition = text_parameter('condition', 'Bipolar')
-    subject = text_parameter('subject', '1000')
+    # hide the in port. Use it as a parameter
+    hidden_in_ports = ['str_par',
+                    'float_par',
+                    'int_par',
+                    'bool_par']
+    str_par = text_parameter('str_par', 'subj1')
+    float_par = float_parameter('float_par', 1.01)
+    int_par = int_parameter('int_par', 2)
+    bool_par = boolean_parameter('bool_par', True)
 
 
 
-# ---------------------------------------------------------------------------
-# PplMapper
-# ---------------------------------------------------------------------------
+##############################################################################
+# DTI Data grabber
+##############################################################################
 
-# This is a special node which allows to run a whole pipeline over a set of
-# parameters. It contains a fixed number of input and output ports that can be
-# mapped to inputs and outputs of the pipeline via corresponding node
-# parameters.
+info = dict(dwi=[['subject_id', 'data']],
+            bvecs=[['subject_id','bvecs']],
+            bvals=[['subject_id','bvals']])
 
-#N_in=5
-#N_out=5
-#in_port_pat="in%s"
-#out_port_pat="out%s"
-#in_map_pat="in_map%s"
-#out_map_pat="out_map%s"
-#tmp_file_pat="tmp_out_%s"
+datasource_iface = nio.DataGrabber(infields=['subject_id'],
+                                               outfields=info.keys())
 
-#in_ports = [in_port_pat % i for i in range(N_in)]
-#out_ports = [out_port_pat % i for i in range(N_out)]
-#in_maps = [in_map_pat % i for i in range(N_in)]
-#out_maps = [out_map_pat % i for i in range(N_out)]
+# currently, earlPipeline doesn't support passing arbitrary python objects as
+# parameters, so the complicated fields like "template_args" etc will be
+# hardcoded here for now (as defaults for interface):
+datasource_iface.inputs.template = "%s/%s"
 
-#def run_pipeline(**kwargs):
-    #"""Builds and runs a pipeline."""
-    #import pickle
-    #from nipype.pipelines.engine import Workflow, Node
-    #from nipype.interfaces.utility import Function
-    #from nipype_wrapper_base import Pipeline
+#datasource_iface.inputs.base_directory = os.path.abspath('../../fsl_course_data/fdt2/')
 
-    ##import ipdb; ipdb.set_trace()
+datasource_iface.inputs.field_template = dict(dwi='%s/%s.nii.gz')
+datasource_iface.inputs.template_args = info
 
-    #wf = Workflow()
 
-    ## store the output of the pipeline temporarily
-    #def write_tmp_out(i, data):
-        #import pickle
-        #with open(tmp_file_pat % i, 'w') as f:
-            #pickle.dump(data, f)
-    #tmp_iface = Function(input_names=['i', 'data'],
-                         #output_names=[],
-                         #function=write_tmp_out)
+class DTIDataSource(Unit):
+    interface = datasource_iface
+    tag = "Sources"
+    instance_name_template = "dtisrc"
 
-    #wf_node = Pipeline.load(kwargs['pipeline_file'])._workflow
-    #wf.add_nodes([wf_node])
+    # only leave "subject_id" as an input port
+    hidden_in_ports = ['ignore_exception',
+                    'raise_on_empty',
+                    'sort_filelist',
+                    'template_args',
+                    'template',
+                    'base_directory',
+                    'field_template']
 
-    ## strange bugfix for a strange bug
-    #getattr(wf_node, 'inputs')
-    #getattr(wf_node, 'outpts')
+    # expose system ports as parameters
+    ignore_exception = boolean_parameter('ignore_exception', False)
+    raise_on_empty = boolean_parameter('raise_on_empty', True)
+
+    # expose some ports as parameters
+    subject_id = text_parameter('subject_id', 'subj1')
+    base_directory = text_parameter('base_directory', '/home/dmytro/work/TU/fsl_course_data/fdt2')
+    sort_filelist = boolean_parameter('sort_filelist', True)
+
+
+
+##############################################################################
+# FSL ROI extraction
+##############################################################################
+
+roi_iface = fsl.ExtractROI()
+
+class ROIExtractor(Unit):
+    interface = roi_iface
+    tag = "Processing"
+    instance_name_template = "roi"
+
+    hidden_in_ports = ['terminal_output',
+                    'args',
+                    'crop_list',
+                    'environ',
+                    'ignore_exception',
+                    'output_type',
+                    'roi_file',
+                    't_min',
+                    't_size',
+                    'x_min',
+                    'x_size',
+                    'y_min',
+                    'y_size',
+                    'z_min',
+                    'z_size']
+
+    ignore_exception = boolean_parameter('ignore_exception', False)
+    terminal_output = Parameter('terminal_output', 'dropdown', str, 'stream',
+            items = ['stream', 'allatonce', 'file', 'none'])
+    args = text_parameter('args', '')
+    output_type = Parameter('output_type', 'dropdown', str, 'NIFTI',
+            items = ['NIFTI_PAIR', 'NIFTI_PAIR_GZ', 'NIFTI_GZ', 'NIFTI'])
+
+    # these integer parameters are undefined by default. Currently, the
+    # properties descriptor necessarily requires a default value. I'll have to
+    # implement undefined parameters before adding all these fields. For now,
+    # I'll just use tutorial values as default
+    t_min = int_parameter('t_min', 0)
+    t_size = int_parameter('t_size', 1)
+
+
+
+##############################################################################
+# FSL Brain Extraction Tool
+##############################################################################
+
+bet_iface = fsl.BET()
+
+class BrainExtractor(Unit):
+    interface = bet_iface
+    tag = "Processing"
+    instance_name_template = "bet"
+
+    hidden_in_ports = ['remove_eyes',
+                     'no_output',
+                     'out_file',
+                     'functional',
+                     'radius',
+                     'threshold',
+                     'surfaces',
+                     't2_guided',
+                     'ignore_exception',
+                     'vertical_gradient',
+                     'frac',
+                     'reduce_bias',
+                     'args',
+                     'padding',
+                     'mesh',
+                     'robust',
+                     'center',
+                     'outline',
+                     'skull',
+                     'mask',
+                     'terminal_output',
+                     'environ',
+                     'output_type']
+
+    # common stuff
+    ignore_exception = boolean_parameter('ignore_exception', False)
+    terminal_output = Parameter('terminal_output', 'dropdown', str, 'stream',
+            items = ['stream', 'allatonce', 'file', 'none'])
+    args = text_parameter('args', '')
+    output_type = Parameter('output_type', 'dropdown', str, 'NIFTI',
+            items = ['NIFTI_PAIR', 'NIFTI_PAIR_GZ', 'NIFTI_GZ', 'NIFTI'])
+
+    # again, exposing only the tutorial options for now
+    mask = boolean_parameter('mask', True)
+    frac = float_parameter('frac', 0.34)
+
+
+
+##############################################################################
+# FSL DTI fitting routine
+##############################################################################
+
+dti_iface = fsl.DTIFit()
+
+class DTIFitter(Unit):
+    interface = dti_iface
+    tag = "Processing"
+    instance_name_template = "dtifit"
+
+    hidden_in_ports = ['ignore_exception',
+                     'min_x',
+                     'min_y',
+                     'min_z',
+                     'args',
+                     'terminal_output',
+                     'environ',
+                     'max_x',
+                     'little_bit',
+                     'sse',
+                     'output_type',
+                     'max_z',
+                     'cni',
+                     'save_tensor',
+                     'max_y']
     
-    ## set the values of the input port to the port of the loaded pipeline
-    ## according to specified maps
-    #for idx, imap in enumerate(in_maps):
-        #node, port = imap.split(".")
-        #if imap != 'unset':
-            #getattr(wf_node.inputs, node).set(**{port:kwargs[in_port_pat % i]})
-
-    #for idx, omap in enumerate(out_maps):
-        #node_name, port = omap.split(".")
-        #if omap != 'unset':
-            #tmp_node = Node(tmp_iface, 'tmp_%s'%idx)
-            #tmp_node.inputs.i = idx
-            #wf.add_nodes([tmp_node])
-            #wf.connect(wf_node, port, tmp_node, 'data')
-
-    #wf.run()
-
-    #outputs = []
-    #for idx, omap in enumerate(out_maps):
-        #node, port = omap.split(".")
-        #if omap != 'unset':
-            #with open(tmp_file_pat % idx) as f:
-                #data = pickle.load(f)
-            #outputs.append(data)
-        #else:
-            #outputs.append(None)
-
-    #return outputs
-
-#def foo(**kwargs):
-    #import logging
-    #logger = logging.getLogger('asdfg')
-    #logger.info(kwargs['in_map2'])
-    #return range(5)
-
-#iface = Function(input_names=in_ports+in_maps+out_maps,
-                 #output_names=out_ports,
-                 #function=foo)
-
-#in_params = [(name, {'name':name, 'parameter_type': 'text', 'value_type':str, 'default_value':'unset'}) for name in in_maps]
-#out_params = [(name, {'name':name, 'parameter_type': 'text', 'value_type':str, 'default_value':'unset'}) for name in out_maps]
-
-#PipelineMapper = create_nipype_unit_class('PplMapper', iface, tag="Utility",
-        #node_type=MapNode,
-        #parameters=in_params+out_params,
-        #node_attrs={'iterfield': in_ports})
+    ignore_exception = boolean_parameter('ignore_exception', False)
+    terminal_output = Parameter('terminal_output', 'dropdown', str, 'stream',
+            items = ['stream', 'allatonce', 'file', 'none'])
+    args = text_parameter('args', '')
+    base_name = text_parameter("base_name", "dtifit_")
 
 
-# -----------------------------------------------------------------------------
-# DataLogger
-# -----------------------------------------------------------------------------
 
-def data_logger_func(logger_name, data):
-    import logging
-    logger = logging.getLogger(logger_name)
-    logger.info(str(data))
+##############################################################################
+# Data Sink
+##############################################################################
 
-data_logger_iface = Function(input_names=['logger_name', 'data'],
-                 output_names=[],
-                 function = data_logger_func)
+datasink_interface = nio.DataSink()
 
-class DataLogger(Unit):
-    interface=data_logger_iface
-    tag='Sinks'
+class DataSink5(Unit):
+    interface = datasink_interface
+    tag = "Sinks"
+    instance_name_template = "datasink"
+    redirected_ports_number = {'in': 2, 'out': 0}
 
-    logger_name = Parameter(name='logger_name',
-            parameter_type='text',
-            value_type=str,
-            default_value='data_logger')
 
-#DataLogger = create_nipype_unit_class('DataLogger', iface, tag="Sinks")
 
-#DataLogger = create_nipype_unit_class('DataLogger', iface, tag="Sinks",
-        #parameters = [('logger_name', {
-            #'name': 'logger_name',
-            #'parameter_type': 'text',
-            #'value_type': str,
-            #'default_value': 'data_logger',
-            #})])
 
+
+#infosource = pe.Node(interface=util.IdentityInterface(fields=['subject_id']),
+                     #name="infosource")    
+#infosource.inputs.subject_id = "subj1"
+
+#datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id'],
+                                               #outfields=info.keys()), name = 'datasource')
+#datasource.inputs.template = "%s/%s"
+
+#datasource.inputs.base_directory = os.path.abspath('../../fsl_course_data/fdt2/')
+
+#datasource.inputs.field_template = dict(dwi='%s/%s.nii.gz',
+                                        #seed_file="%s.bedpostX/%s.nii.gz",
+                                        #target_masks="%s.bedpostX/%s.nii.gz")
+#datasource.inputs.template_args = info
+#datasource.inputs.sort_filelist = True
+
+#computeTensor = pe.Workflow(name='computeTensor')
+
+#fslroi = pe.Node(interface=fsl.ExtractROI(),name='fslroi')
+#fslroi.inputs.t_min=0
+#fslroi.inputs.t_size=1
+
+#bet = pe.Node(interface=fsl.BET(),name='bet')
+#bet.inputs.mask=True
+#bet.inputs.frac=0.34
+
+#eddycorrect = create_eddy_correct_pipeline('eddycorrect')
+#eddycorrect.inputs.inputnode.ref_num=0
+
+#dtifit = pe.Node(interface=fsl.DTIFit(),name='dtifit')
+
+#computeTensor.connect([
+                        #(infosource,datasource,[('subject_id', 'subject_id')]),
+                        #(datasource,fslroi,[('dwi','in_file')]),
+                        #(datasource, dtifit, [('bvals', 'bvals'),
+                                              #('bvecs', 'bvecs')]),
+                        #(datasource, eddycorrect, [('dwi', 'inputnode.in_file')]),
+                        #(fslroi,bet,[('roi_file','in_file')]),
+                        #(eddycorrect, dtifit,[('outputnode.eddy_corrected','dwi')]),
+                        #(infosource, dtifit,[['subject_id','base_name']]),
+                        #(bet,dtifit,[('mask_file','mask')])
+                      #])
 
 def get_unit_types():
-    #return [DataSource, PipelineMapper, DataLogger]
-    return [RangeSource, NbackSource, DataLogger]
+    # list of all classes that are visible from the GUI
+    return [PrimitiveSource,
+            DTIDataSource,
+            ROIExtractor,
+            BrainExtractor,
+            DTIFitter,
+            DataSink5]
 
